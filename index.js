862e212832db6f8f21cada35ea685a66c572ccd7
KlumsyKamikaze <ishaan@spysoft.com>
2022-05-18 01:40:01 +0530

    fix

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.use(
 );
 
 connectToMongoDB();
-app.listen(5200, () => console.log("Server ready"));
+app.listen(process.env.PORT || 5200, () => console.log("Server ready"));
 
 app.post("/", async (req, res) => {
   try {
